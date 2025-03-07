21c8e443db83463cc7e61ef7ab356a1c28c05f3f
Anish Ghimire <anishghimire862@gmail.com>
2025-03-07 15:58:40 +0545

    init wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  nitro: {
+    preset: "cloudflare"
+  },
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
